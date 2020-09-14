8013866e9ba606d2da4fb96d0f7df5f72f5717b3
unknown <sudhanshu221b@gmail.com>
2020-09-14 09:33:19 +0530

    added basic color palette

---
+++ src/theme/theme.js
@@ -0,0 +1,15 @@
+export default colors = {
+  // Background light mode
+  //white
+  bg1: "#FBFFFE",
+  // very light blue
+  bg2: "#85E0FF",
+  // some egyptian blue
+  bg3: "#0E34A0",
+
+  // text light mode
+  // some xiketic black
+  text1: "#1A090D",
+  // some ebony grey
+  text2: "#636B61",
+};
