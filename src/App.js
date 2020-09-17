4dfbe0b26c7da36fc44de0c8ce079c541c5382e6
unknown <sudhanshu221b@gmail.com>
2020-09-17 07:26:56 +0530

    created /home redirectc route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  redirect,
+} from "react-router-dom";
 
-import {Home, Login, Register} from './pages'
+import { Home, Login, Register, Diary } from "./pages";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/home" exact children={<Redirect to="/" />} />
         <Route path="/login" exact component={Login} />
         <Route path="/register" exact component={Register} />
+        <Route path="/diary" exact component={Diary} />
       </Switch>
     </Router>
   );
