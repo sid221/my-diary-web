2beade1684b7789644087e569d79e7de47a5e7ca
sid221 <sudhanshu221b@gmail.com>
2020-10-17 02:05:54 +0530

    state update on note delete/create

--- src/redux/diary/diaryReducer.js
+++ src/redux/diary/diaryReducer.js
@@ -4,6 +4,10 @@ import {
   FETCH_DIARY_NOTES_FAILED,
 } from "./diaryActionTypes";
 import { USER_LOGOUT } from "../user/userActionTypes";
+import {
+  CREATE_NOTE_SUCCESS,
+  DELETE_NOTE_SUCCESS,
+} from "../note/noteActionTypes";
 
 const initialState = {
   notes: null,
@@ -34,6 +38,23 @@ const diary = (state = initialState, action) => {
     //USER LOGOUT
     case USER_LOGOUT:
       return {};
+
+    // On New Note Creation Add to note list
+    case CREATE_NOTE_SUCCESS:
+      return !!state.notes
+        ? { ...state, notes: [...state.notes, action.payload.createdNote] }
+        : { ...state };
+
+    // On delete a Note
+    case DELETE_NOTE_SUCCESS:
+      return !!state.notes
+        ? {
+            ...state,
+            notes: state.notes.filter(
+              (note) => note.noteId !== action.payload.noteId
+            ),
+          }
+        : { ...state };
 
     default:
       return { ...state };
