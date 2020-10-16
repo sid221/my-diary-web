8ea47b1b4e0e86a8a2816a98f74f18bed4bdaf89
sid221 <sudhanshu221b@gmail.com>
2020-10-17 02:04:39 +0530

    integrated create note api

--- src/pages/diary/createNote.js
+++ src/pages/diary/createNote.js
@@ -1,48 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import dayjs from "dayjs";
-import { Prompt } from "react-router-dom";
+import { Prompt, useHistory } from "react-router-dom";
+
+import { useSelector, useDispatch } from "react-redux";
+import { createNote } from "../../redux/note/noteActions";
 
 import DatePicker from "react-date-picker";
-
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-import "react-quill/dist/quill.core.css";
-import "react-quill/dist/quill.bubble.css";
 
 import DiaryNavbar from "../../layout/diaryNavbar";
+import ShowError from "../../layout/ShowError";
+import PulseLoader from "react-spinners/PulseLoader";
+import colors from "../../styles/theme";
 import { Input, Button } from "../../styles/styledElement";
 import {
   StyledDiaryLayout,
   StyledDiaryHead,
   StyledDiaryBody,
   StyledNotesContainer,
+  StyledNoteBody,
   StyledNoteTitle,
   StyledNoteDate,
   StyledNoteEditorContainer,
 } from "../../layout/diaryLayout";
 
 const CreateNote = () => {
-  //   const history = useHistory();
-  const [noteBody, setNoteBody] = useState("");
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const { createNoteLoading, newNoteData, createNoteError } = useSelector(
+    (state) => state.note
+  );
   const [noteTitle, setNoteTitle] = useState("");
   const [noteDate, setNoteDate] = useState(new Date());
+  const [noteBody, setNoteBody] = useState(null);
+  const [noteStyle, setNoteStyle] = useState({});
   const [isBlock, setIsBlock] = useState(true);
 
-  const handleNoteSave = (e) => {
-    // e.preventDefault();
-    console.log("dfgdkgjko");
-    const updatedNote = {
-      title: noteTitle,
-      body: noteBody,
-      createdAt: noteDate,
-    };
-    console.log(isBlock);
-    setTimeout(() => {
-      setIsBlock(false);
-    }, 1000);
-    console.log("Note Saved with data: ", updatedNote);
+  useEffect(() => {
+    if (!!newNoteData && !createNoteLoading) setIsBlock(false);
+  }, [newNoteData, setIsBlock, createNoteLoading]);
 
-    // history.push(`/note/${note.noteId}`);
+  const handleNoteSave = (e) => {
+    let noteData = {
+      title:
+        noteTitle || `Memoir of ${dayjs(Date()).format("ddd, DD MMM YYYY")}`,
+      body: noteBody,
+      createdAt: dayjs(noteDate).format("DD MMM YYYY"),
+      styles: noteStyle || {},
+    };
+
+    // console.log(noteData);
+    // console.log(history);
+    setIsBlock(false);
+    dispatch(createNote(noteData, history));
+  };
+
+  const handleSetStyle = () => {
+    // let style = {
+    //   bg:
+    //   borderLeft:
+    //   dateBg:
+    // }
+    setNoteStyle({});
   };
 
   return (
@@ -60,45 +80,58 @@ const CreateNote = () => {
             onClick={handleNoteSave}
             noBackground
             className="note-edit-save"
+            disabled={createNoteLoading}
           >
-            <i className="fas fa-save" />
-            <span> Save</span>
+            {createNoteLoading ? (
+              <PulseLoader color={colors.bg3} />
+            ) : (
+              <>
+                <i className="fas fa-save" />
+                <span> Save</span>
+              </>
+            )}
           </Button>
         </StyledDiaryHead>
         <StyledNotesContainer>
-          <StyledNoteTitle>
-            <label htmlFor="note-title">
-              <h2>Title:</h2>
-            </label>
-            <Input
-              name="note-title"
-              value={noteTitle}
-              placeholder={`Enter your title, Default will be "Memoir of ${dayjs(
-                Date()
-              ).format("ddd, DD MMM YYYY")}"`}
-              onChange={(e) => setNoteTitle(e.target.value)}
-            />
-          </StyledNoteTitle>
-          <StyledNoteDate>
-            <label htmlFor="note-date">
-              <h3>Date:</h3>
-            </label>
-            <div className="datepicker-container">
-              <DatePicker
-                value={noteDate}
-                onChange={setNoteDate}
-                clearIcon={null}
-                calendarIcon={<i className="fas fa-calendar-alt" />}
+          {!!createNoteError && <ShowError error={createNoteError} />}
+          <StyledNoteBody>
+            <StyledNoteTitle>
+              <label htmlFor="note-title">
+                <h2>Title:</h2>
+              </label>
+              <Input
+                name="note-title"
+                value={noteTitle}
+                placeholder={`Enter your title, Default will be "Memoir of ${dayjs(
+                  Date()
+                ).format("ddd, DD MMM YYYY")}"`}
+                onChange={(e) => setNoteTitle(e.target.value)}
               />
-            </div>
-          </StyledNoteDate>
-          <StyledNoteEditorContainer>
-            <ReactQuill
-              value={noteBody}
-              onChange={(val) => setNoteBody(val)}
-              placeholder="Write your story here..."
-            />
-          </StyledNoteEditorContainer>
+            </StyledNoteTitle>
+            <StyledNoteDate>
+              <label htmlFor="noteDate">
+                <h3>Date:</h3>
+              </label>
+              <div className="datepicker-container">
+                <DatePicker
+                  name="noteDate"
+                  value={noteDate}
+                  onChange={setNoteDate}
+                  format="dd-MM-y"
+                  clearIcon={null}
+                  calendarIcon={<i className="fas fa-calendar-alt" />}
+                />
+              </div>
+            </StyledNoteDate>
+            <StyledNoteEditorContainer>
+              <ReactQuill
+                value={noteBody}
+                theme="snow"
+                onChange={(val) => setNoteBody(val)}
+                placeholder="Write your story here..."
+              />
+            </StyledNoteEditorContainer>
+          </StyledNoteBody>
         </StyledNotesContainer>
       </StyledDiaryBody>
     </StyledDiaryLayout>
