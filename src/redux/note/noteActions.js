665e31088a3c1834511e8206bbbc706763f1c2db
sid221 <sudhanshu221b@gmail.com>
2020-10-17 02:08:01 +0530

    delete/create note api integrated

--- src/redux/note/noteActions.js
+++ src/redux/note/noteActions.js
@@ -6,9 +6,12 @@ import {
   FETCH_NOTE_LOADING,
   FETCH_NOTE_SUCCESS,
   FETCH_NOTE_FAILED,
-  // CREATE_NOTE_LOADING,
-  // CREATE_NOTE_SUCCESS,
-  // CREATE_NOTE_FAILED,
+  CREATE_NOTE_LOADING,
+  CREATE_NOTE_SUCCESS,
+  CREATE_NOTE_FAILED,
+  DELETE_NOTE_LOADING,
+  DELETE_NOTE_SUCCESS,
+  DELETE_NOTE_FAILED,
   // SAVE_NOTE_LOADING,
   // SAVE_NOTE_SUCCESS,
   // SAVE_NOTE_FAILED,
@@ -42,4 +45,61 @@ const fetchNote = (noteId) => (dispatch) => {
     });
 };
 
-export { setNote, fetchNote };
+const createNoteLoading = () => {
+  return { type: CREATE_NOTE_LOADING };
+};
+const createNoteSuccess = (payload) => {
+  return { type: CREATE_NOTE_SUCCESS, payload };
+};
+const createNoteFailed = (payload) => {
+  return { type: CREATE_NOTE_FAILED, payload };
+};
+const createNote = (noteData, history) => (dispatch) => {
+  dispatch(createNoteLoading());
+  axios
+    .post(`${API}/notes`, noteData, {
+      headers: {
+        authorization: localStorage.token,
+      },
+    })
+    .then(({ data }) => {
+      console.log(data);
+      dispatch(createNoteSuccess(data));
+      history.push(`/note/${data.createdNote.noteId}`);
+    })
+    .catch((err) => {
+      console.log(err);
+      return dispatch(createNoteFailed(err));
+    });
+};
+
+// Delete Note Action
+const deleteNoteLoading = () => {
+  return { type: DELETE_NOTE_LOADING };
+};
+const deleteNoteSuccess = (payload) => {
+  return { type: DELETE_NOTE_SUCCESS, payload };
+};
+const deleteNoteFailed = (payload) => {
+  return { type: DELETE_NOTE_FAILED, payload };
+};
+const deleteNote = (noteId, history) => (dispatch) => {
+  dispatch(deleteNoteLoading());
+  axios
+    .delete(`${API}/notes/${noteId}`, {
+      headers: {
+        authorization: localStorage.token,
+      },
+    })
+    .then(({ data }) => {
+      // console.log(data);
+      dispatch(deleteNoteSuccess(data));
+      history.push(`/diary`);
+    })
+    .catch((err) => {
+      console.log(err);
+      return dispatch(deleteNoteFailed(err));
+    });
+};
+
+export { setNote, fetchNote, createNote, deleteNote };
--- src/redux/note/noteReducer.js
+++ src/redux/note/noteReducer.js
@@ -4,9 +4,12 @@ import {
   FETCH_NOTE_LOADING,
   FETCH_NOTE_SUCCESS,
   FETCH_NOTE_FAILED,
-  // CREATE_NOTE_LOADING,
-  // CREATE_NOTE_SUCCESS,
-  // CREATE_NOTE_FAILED,
+  CREATE_NOTE_LOADING,
+  CREATE_NOTE_SUCCESS,
+  CREATE_NOTE_FAILED,
+  DELETE_NOTE_LOADING,
+  DELETE_NOTE_SUCCESS,
+  DELETE_NOTE_FAILED,
   // SAVE_NOTE_LOADING,
   // SAVE_NOTE_SUCCESS,
   // SAVE_NOTE_FAILED,
@@ -18,6 +21,12 @@ const initialState = {
   currentNote: null,
   noteLoading: false,
   noteError: null,
+  createNoteLoading: false,
+  createNoteData: null,
+  createNoteError: null,
+  deleteNoteLoading: false,
+  deleteNoteData: null,
+  deleteNoteError: null,
 };
 
 const note = (state = initialState, action) => {
@@ -36,7 +45,42 @@ const note = (state = initialState, action) => {
     // LOGOUT USER DATA CLEANING
     case USER_LOGOUT:
       return {};
-      
+
+    // CREATE NEW NOTE
+    case CREATE_NOTE_LOADING:
+      return { ...state, createNoteLoading: true };
+    case CREATE_NOTE_SUCCESS:
+      return {
+        ...state,
+        createNoteLoading: false,
+        createNoteError: null,
+        createNoteData: action.payload,
+        currentNote: action.payload.createdNote,
+      };
+    case CREATE_NOTE_FAILED:
+      return {
+        ...state,
+        createNoteLoading: false,
+        createNoteError: action.payload,
+      };
+
+    // Delete Note
+    case DELETE_NOTE_LOADING:
+      return { ...state, deleteNoteLoading: true };
+    case DELETE_NOTE_SUCCESS:
+      return {
+        ...state,
+        deleteNoteLoading: false,
+        deleteNoteError: null,
+        deleteNoteData: action.payload,
+      };
+    case DELETE_NOTE_FAILED:
+      return {
+        ...state,
+        deleteNoteLoading: false,
+        deleteNoteError: action.payload,
+      };
+
     default:
       return { ...state };
   }
